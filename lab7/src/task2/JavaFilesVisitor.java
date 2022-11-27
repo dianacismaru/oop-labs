730a84915a4db43d91a1d0dc0586f374ea451f96
Diana Cismaru <cismaru_diana02@yahoo.com>
2022-11-27 20:25:10 +0200

    Implement solution for task2

--- lab7/src/task2/JavaFilesVisitor.java
+++ lab7/src/task2/JavaFilesVisitor.java
@@ -2,6 +2,7 @@ package task2;
 
 import java.io.IOException;
 import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.util.ArrayList;
 
 /**
@@ -9,16 +10,20 @@ import java.util.ArrayList;
  */
 public class JavaFilesVisitor extends SimpleFileVisitor<Path> {
 
-    private ArrayList<Path> javaFiles;
+    private ArrayList<Path> javaFiles = new ArrayList<>();
 
     public final ArrayList<Path> getJavaFiles() {
         return javaFiles;
     }
 
-    /**
-     * TODO - override the visitFile(Path file, BasicFileAttributes attr) method of the SimpleFileVisitor.
-     * Add to javaFiles all the Java related files: the ones with .java and .class extensions.
-     */
+    @Override
+    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+        if (file.endsWith(".class") || file.endsWith(".java"))
+            this.javaFiles.add(file);
+
+        return super.visitFile(file, attrs);
+    }
+
     public static void main(String[] args) {
 
         Path startingDir = Paths.get("./src/lab7");
@@ -32,7 +37,11 @@ public class JavaFilesVisitor extends SimpleFileVisitor<Path> {
             Files.walkFileTree(startingDir, filesVisitor);
             ArrayList<Path> javaFiles = filesVisitor.getJavaFiles();
 
-            // TODO: Print the number of files visited and their names
+            System.out.println("Number of files visited: " + javaFiles.size());
+
+            for (Path filePath: javaFiles) {
+                System.out.println(filePath.getFileName());
+            }
 
         } catch (IOException e) {
             e.printStackTrace();
